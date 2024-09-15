b8810cde30f6b38fa1e849f2530faeb2ed5685c3
adicomdotir <adicom.ir@gmail.com>
2024-09-15 13:07:04 +0330

    feat: add icon and change logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+
 import 'app.dart';
 import 'bloc_observer.dart';
 import 'injection_container.dart' as di;
